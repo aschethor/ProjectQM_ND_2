524b7fee02670039973c17e9159c5001f3e0d0e6
Nils <ni.wa@live.de>
2017-12-12 20:44:09 +0100

    presentation data generation files

---
+++ Presentation/HarmonicPotential.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <math.h>
+#include <time.h>
+#include "../System.h"
+#include "../Particle.h"
+#include "../Utils.h"
+#include <fstream>
+
+using namespace std;
+
+ofstream output_file;
+
+void observable(System& system){
+    // cout<<"here in observable: ";
+    for(int i=0;i<system.particles[0].beads.size();i++){
+        //cout<<system.particles[0].beads[i].x<<" ";
+        output_file<<""<<system.particles[0].beads[i].x<<endl;
+    }
+}
+
+double external_potential(Bead bead){
+    double w = 1;
+    return w*w/2*bead.x*bead.x;
+}
+
+double my_calc_external_term(Particle& particle){
+    int n = particle.beads.size();
+    double term = 0;
+    for(int i=0;i<n;i++){
+        term += external_potential(particle.beads[i]);
+    }
+    return term/n;
+}
+
+double my_calc_internal_term(Particle& particle1,Particle& particle2){
+    return 0;
+}
+
+/*
+ * steps with "modes"
+ * -> much better convergence
+ */
+void my_step_1D(Particle* particle,vector<double> randoms){
+    if(randoms.size()==0){
+        double alpha = 1;
+        double x = (rand_u()*2-1)*alpha;
+
+        // excite "modes" of bead-ring
+        // int mode = min(-(int)log2(rand_u()),particle->beads.size()/2);
+        int mode = get_mode(particle->beads.size()/2);
+        // cout<<" excited mode: "<<mode<<endl;
+        double phase = rand_u()*M_PI/2;
+        for(int i=0;i<particle->beads.size();i++){
+            double factor = sin(i*mode*2*M_PI/particle->beads.size()+phase);
+            particle->beads[i].x += factor*x;
+        }
+    }
+}
+
+void quantum_double_well(double temperature, int n_beads){
+    cout<<"Simulation: Temperature = "<<temperature<<"; Number of Beads = "<<n_beads<<endl;
+    string filename = "C:\\Users\\NiWa\\Documents\\MATLAB\\Project CQM\\data\\presentation\\harmonic_oscillator_qm_T_"
+                      + to_string(temperature)+"_N_"+to_string(n_beads)+".csv";
+    output_file.open(filename);
+    System system(&my_calc_internal_term,&my_calc_external_term);
+    system.set_T(temperature);
+    system.add_particle(Particle(1,'x',n_beads,&my_step_1D));
+    system.monte_carlo(10000);
+    system.add_observable(&observable);
+    system.monte_carlo(40000*32/n_beads);
+    cout<<"done. accepted vs rejected samples: "<<system.n_accepted<<" / "<<system.n_rejected<<endl;
+    output_file.close();
+}
+
+int main() {
+    cout<<"Harmonic Oscillator"<<endl;
+    srand (time(NULL));
+    double temperatures[] = {0.1,1};
+    int n_beads[] = {1,2,4,8,16,32,64};
+    for(double t:temperatures){
+        for(int n:n_beads){
+            quantum_double_well(t, n);
+        }
+    }
+    return 0;
+}
